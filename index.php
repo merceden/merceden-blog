b6e9845d6b838a033b12b2d376812d40348ff4cb
den <den@MacBook-Pro-den.local>
2017-12-26 22:43:24 +0200

    Revert "Initial commit"
    This reverts commit 60e6f26ffc18a714fcf2d5c1aeca0473f77d4ce7.

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-  require_once '/Applications/MAMP/htdocs/lib/index.php';
-?>
--- lib/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-		
-    require_once '/Applications/MAMP/htdocs/model/posts.php';
-	
-	$posts = posts();
-		
-	require_once '/Applications/MAMP/htdocs/view/index.php';
